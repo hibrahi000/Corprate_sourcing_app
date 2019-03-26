19b0191b043eaffb5004096226f7e862dd5479a5
Hashmat Ibrahimi <hashmat.ibrahimi@lc.cuny.edu>
2019-03-26 12:24:36 -0400

    same thing 2'

--- controllers/controller.js
+++ controllers/controller.js
@@ -255,7 +255,7 @@ module.exports = (app) =>{
                     vendor.findOne(query).then(theVendor =>{
                     const mailOptionsVendForm = {
                         from: `${vendorName} <${theVendor.Email}>`,// sender address
-                        to: '<tech@abhpharma.com>, <purchase@abhpharma.com>,<test-favxv@mail-tester.com>,<hashmat.ibrahimi@lc.cuny.edu>', // list of receivers
+                        to: '<tech@abhpharma.com>, <purchase@abhpharma.com>,<st-3-a7kw58ztv1@glockapps.com>,<hashmat.ibrahimi@lc.cuny.edu>', // list of receivers
                         subject: `${vendorName} Request Submission For ${material}`,
                         text:        `
                         NEW MATERIAL: ${isNew} <br>   

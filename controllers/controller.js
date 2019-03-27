5a3d0df7aed9be86ac7e48d027e4cf07f386fa48
Hashmat Ibrahimi <hashmat.ibrahimi@lc.cuny.edu>
2019-03-27 11:36:13 -0400

    changed purchase to purchasing email err

--- controllers/controller.js
+++ controllers/controller.js
@@ -250,7 +250,7 @@ module.exports = (app) =>{
                     vendor.findOne(query).then(theVendor =>{
                     const mailOptionsVendForm = {
                         from: `${vendorName} <${theVendor.Email}>`,// sender address
-                        to: '<purchase@abhpharma.com>',
+                        to: '<purchasing@abhpharma.com>',
                         cc: '<tech@abhpharma.com>', // list of receivers
                         subject: `${vendorName} Request Submission For ${material}`,
                         text:        `
@@ -453,7 +453,7 @@ module.exports = (app) =>{
 
                                                                 const mailOptionsVendUnsubscibeNewDel = {
                                                                     from: `${vendorName} <${vend.Email}>`, // sender address
-                                                                    to: ' <tech@abhpharma.com>,<purchase@abhpharma.com', // list of receivers
+                                                                    to: ' <tech@abhpharma.com>,<purchasing@abhpharma.com', // list of receivers
                                                                     subject: `${vendorName} Unsubscription For ${material} ---MATERIAL REMOVED---`,
                                                                     text: `Since ${vendorName} requested to be removed from the email chain for material: ${material}, we dont have any vendors that support it so it was removed from the database. <br><br> If the vendor contacts you to undo this change you can always re-add the material in the <em>Modify Vendor<em> Page in the purchase app. All you will have to do is: <br> 1) search for the vendors name<br>2)Add the material ** Spaces should be replaced with dashes and multiple materials should be comma seperated AND no spaces before or after the commas <br> 3)Then click save
                                                                     
@@ -487,7 +487,7 @@ module.exports = (app) =>{
                                                         else{
                                                             const mailOptionsVendUnsubscibeNew = {
                                                                 from: `${vendorName} <${vend.Email}>`, // sender address
-                                                                to: '<purchase@abhpharma.com>',
+                                                                to: '<purchasing@abhpharma.com>',
                                                                 cc:'<tech@abhpharma.com>', // list of receivers
                                                                 subject: `${vendorName} Unsubscription For ${material}`,
                                                                 text: `Since ${vendorName} requested to be removed from the email chain for material: ${material}. <br><br> If the vendor contacts you to undo this change you can always re-add the material in the <em>Modify Vendor<em> Page in the purchase app. All you will have to do is: <br> 1) search for the vendors name<br>2)Add the material ** Spaces should be replaced with dashes and multiple materials should be comma seperated AND no spaces before or after the commas <br> 3)Then click save
@@ -533,7 +533,7 @@ module.exports = (app) =>{
                                             console.log('else');
                                             const mailOptionsUnsubscribe = {
                                                 from: `${vendorName} <${vendors.Email}>`, // sender address
-                                                to: '<purchase@abhpharma.com>', 
+                                                to: '<purchasing@abhpharma.com>', 
                                                 cc: '<tech@abhpharma.com>',// list of receivers
                                                 subject: `${vendorName} Request Removal From Email Chain For New Material: ${material}`,
                                                 text : 
@@ -794,7 +794,7 @@ app.get('/', urlencodedParser,(req,res) =>{
                     
                     
                         const mailOptionsReq = {
-                            from: 'ABH Purchase Dept. <purchase@abhpharma.com>', // sender address
+                            from: 'ABH Purchase Dept. <purchasinge@abhpharma.com>', // sender address
                             to: vendorContact[i], // list of receivers
                             //${vendorContact[i]},
                             subject: `ABH-Pharma Quote Request for ${material} `, // Subject line

f3174be0c70aa0391c056eece3e3d41b3a588334
Hashmat Ibrahimi <hashmat.ibrahimi@lc.cuny.edu>
2019-04-22 10:24:55 -0400

    Security measures

--- controllers/config/keys.js
+++ controllers/config/keys.js
@@ -5,6 +5,25 @@ const jwtPrivateKey = 'PassTigerSeasonPen0711!@#HashNat000';
 const session = {
 	cookieKey: 'TigerPassSeasonPen0711!@#HashNat000'
 };
+
+
+const herokuRecoveryCodes = [
+	
+	
+	'06c0104f1d42a172',
+	'24c22b400f64e83a',
+	'42d0015fa6bd72d8',
+	'43474517402f20a5',
+	'46aa0a17feb4046a',
+	'4a22c7148e7cebe6',
+	'9176b239ad8eff1f',
+	'b177d9e925883ac8',
+	'd4a4af6779cec417',
+	'eedae256390143bb'
+];
+
+
+
 module.exports = {
 	default_vendor_key: default_vendor_key,
 
